e843e0686099a340b497552f673896946744d9a5
Daniel Zuziak <daniel.zuziak@gmail.com>
2018-11-14 13:05:19 +0100

    Trying to denoise lungs

--- Lab3/noise_and_artifacts.py
+++ Lab3/noise_and_artifacts.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.color import rgb2gray
-from skimage.morphology import closing, square, disk, convex_hull_image
+from skimage.morphology import opening, closing, square, disk, convex_hull_image
 from skimage.exposure import equalize_adapthist
 from skimage.restoration import denoise_tv_chambolle
 from skimage.filters.rank import equalize
 from skimage.filters import gaussian, threshold_otsu, sobel
+from skimage.feature import shape_index
 
 
 def beautify_head():
@@ -47,13 +48,36 @@ def beautify_head():
     plt.show()
 
 
-# def beautify_lungs():
-#     lungs_disrupted = plt.imread('lungs.jpg').astype(np.float64)
-#     print(lungs_disrupted.shape)
-#     print(lungs_disrupted.dtype)
-#     plt.subplot(222)
-#     plt.imshow(lungs_disrupted, cmap='gray')
-#     plt.show()
+def beautify_lungs():
+    lungs = plt.imread('lungs.jpg').astype(np.float32) / 256.0
+
+    plt.subplot(231)
+    plt.imshow(lungs, cmap='gray')
+
+    lungs_shape = shape_index(lungs)
+
+    plt.subplot(234)
+    plt.imshow(np.clip(lungs_shape, 0.2, 0.8), cmap='gray')
+
+    lungs_denoised = denoise_tv_chambolle(lungs, weight=0.1)
+
+    lungs_shape = shape_index(lungs_denoised)
+
+    plt.subplot(235)
+    plt.imshow(np.clip(lungs_shape, 0., 0.8), cmap='gray')
+
+    lungs_shape_clipped = (lungs_shape > 0.3) & (lungs_shape < 0.55)
+
+    plt.subplot(236)
+    plt.imshow(lungs_shape_clipped, cmap='gray')
+
+    plt.subplot(233)
+    plt.imshow(closing(opening(lungs_shape_clipped), selem=disk(3)), cmap='gray')
+
+    plt.subplot(232)
+    plt.imshow(closing(lungs_shape_clipped, selem=disk(7)), cmap='gray')
+
+    plt.show()
 
 
 if __name__ == '__main__':
